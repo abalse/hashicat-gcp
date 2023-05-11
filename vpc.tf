82874b1f7f7a7ce8fd24651a540278bf0b55f94f
abalse <abhibalse@gmail.com>
2023-05-11 19:44:44 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,12 @@
-network_name = abhijit-network
-project_id = var.project
-subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
+module "network" {
+  source  = "app.terraform.io/example-org-244af6/network/google"
+  version = "3.4.0"
+  network_name = abhijit-network
+  project_id = var.project
+  subnets = [
+   {
+     subnet_name   = "gaurav-subnet"
+     subnet_ip     = "10.100.10.0/24"
+     subnet_region = var.region
+   }
 ]
